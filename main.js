c99d80414924c528142b2076c7dac1379aff54e0
Torofms <torofms37@gmail.com>
2022-08-07 17:55:31 -0700

    final-js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-let ataqueJugador
-let ataqueEnemigo
+let ataqueJugador;
+let ataqueEnemigo;
+let vidasJugador = 3;
+let vidasEnemigo = 3;
 
 function inicarJuego() {
+    let sectionSeleccionrAtaque = document.getElementById('seleccionar-ataque');
+    sectionSeleccionrAtaque.style.display = 'none';
+
+    let sectionReiniciar = document.getElementById('reiniciar');
+    sectionReiniciar.style.display = 'none';
+
     let botonMascotaJugador = document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)   
     
@@ -13,13 +21,23 @@ function inicarJuego() {
 
     let botonTierra = document.getElementById('boton-tierra')
     botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+
 }
 
 function seleccionarMascotaJugador(){
-let inputHipo = document.getElementById('hipodoge')
-let inputCapi = document.getElementById('capipepo')
-let inputRati = document.getElementById('ratigueya')
-let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let sectionSeleccionrMascota = document.getElementById('seleccionar-mascota');
+    sectionSeleccionrMascota.style.display = 'none';
+
+    let sectionSeleccionrAtaque = document.getElementById('seleccionar-ataque');
+    sectionSeleccionrAtaque.style.display = 'block'
+
+    let inputHipo = document.getElementById('hipodoge')
+    let inputCapi = document.getElementById('capipepo')
+    let inputRati = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     if(inputHipo.checked) {
         spanMascotaJugador.innerHTML = 'Hipodoge'
@@ -28,7 +46,7 @@ let spanMascotaJugador = document.getElementById('mascota-jugador')
     } else if (inputRati.checked) {
         spanMascotaJugador.innerHTML = 'Ratigueya'
     } else {
-    alert ('Selecciona a un jugador')
+        alert ('Selecciona a un jugador')
     }
 
     seleccionarMascotaEnemigo()
@@ -73,18 +91,79 @@ function ataqueAleatorioEnemigo() {
         ataqueEnemigo = 'Tierra'
     }
 
-    crearMensaje();
+    combate();
 }
 
-function crearMensaje() {
+function combate() {
+    let spanVidasJugador = document.getElementById('vidas-jugador');
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo');
+
+    if(ataqueEnemigo == ataqueJugador) {
+        crearMensaje('Empatado');
+    } else if(ataqueJugador == 'Fuego' && ataqueEnemigo == 'Tierra') {
+        crearMensaje('Ganado')
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+
+    }  else if(ataqueJugador == 'Agua' && ataqueEnemigo == 'Fuego') {
+        crearMensaje('Ganado')
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+
+    }  else if(ataqueJugador == 'Tierra' && ataqueEnemigo == 'Agua') {
+        crearMensaje('Ganado')
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+
+    }  else {
+        crearMensaje('Perdido')
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+
+    checkVidas();
+}
+
+function checkVidas() {
+    if(vidasEnemigo == 0) {
+        crearMensajeFinal('¡Ganaste we, te ganaste un mes más suscripcióm Platzi!')
+    } else if (vidasJugador == 0) {
+        crearMensajeFinal('Perdiste we, ni pepe. Sigue intentando!')
+    }
+}
+
+function crearMensaje(resultado) {
     let sectionMensaje = document.getElementById('mensajes');
     
     let parrafo = document.createElement('p');
-    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + " cambiar";
+
+    parrafo.innerHTML = 'Tu mascota atacó con ' + ataqueJugador + ", la mascota del enemigo atacó con " + ataqueEnemigo + ' - Usted a: ' + resultado;
 
     sectionMensaje.appendChild(parrafo);
 }
 
+function crearMensajeFinal(resultadoFinal) {
+    let sectionMensaje = document.getElementById('mensajes');
+    
+    let parrafo = document.createElement('p');
+    parrafo.innerHTML = resultadoFinal
+
+    sectionMensaje.appendChild(parrafo);
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById('reiniciar');
+    sectionReiniciar.style.display = 'block';
+}
+
+function reiniciarJuego() {
+    location.reload();
+}
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
